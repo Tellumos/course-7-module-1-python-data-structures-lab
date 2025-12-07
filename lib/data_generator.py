3002f1a818404bf55b78f55d7b9c9e12661c0aee
mvrz <msv930@gmail.com>
2025-12-06 20:43:09 -0600

    Data Structure in Python

--- lib/data_generator.py
+++ lib/data_generator.py
@@ -1,8 +1,3 @@
-# This module contains functions to lazily generate student data.
-
 def student_generator(student_list, major):
-    """
-    Generate student records filtered by major lazily for memory efficiency
-    using a Python generator.
-    """
-    pass
+
+    return (student for student in student_list if major.lower() in student[2].lower())
